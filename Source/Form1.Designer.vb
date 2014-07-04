2c8239e159e0e26b1221e39d2335ab143aa91689
DanieV <danielyan.vahe@gmail.com>
2014-07-04 23:11:37 +0400

    Project is over here's source code

---
+++ Source/Form1.Designer.vb
@@ -0,0 +1,335 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Vahe_app
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Vahe_app))
+        Me.LB = New System.Windows.Forms.ListBox
+        Me.Button1 = New System.Windows.Forms.Button
+        Me.Button2 = New System.Windows.Forms.Button
+        Me.sqrt = New System.Windows.Forms.Button
+        Me.pow = New System.Windows.Forms.Button
+        Me.log = New System.Windows.Forms.Button
+        Me.sin = New System.Windows.Forms.Button
+        Me.TextBox1 = New System.Windows.Forms.TextBox
+        Me.Button3 = New System.Windows.Forms.Button
+        Me.Cos = New System.Windows.Forms.Button
+        Me.Asin = New System.Windows.Forms.Button
+        Me.Ctan = New System.Windows.Forms.Button
+        Me.Fact = New System.Windows.Forms.Button
+        Me.Button8 = New System.Windows.Forms.Button
+        Me.Button9 = New System.Windows.Forms.Button
+        Me.Acos = New System.Windows.Forms.Button
+        Me.Second = New System.Windows.Forms.TextBox
+        Me.Powx = New System.Windows.Forms.Button
+        Me.nsqrt = New System.Windows.Forms.Button
+        Me.Sum = New System.Windows.Forms.Button
+        Me.Button7 = New System.Windows.Forms.Button
+        Me.Button10 = New System.Windows.Forms.Button
+        Me.ABS = New System.Windows.Forms.Button
+        Me.Button5 = New System.Windows.Forms.Button
+        Me.Button6 = New System.Windows.Forms.Button
+        Me.Button11 = New System.Windows.Forms.Button
+        Me.Button13 = New System.Windows.Forms.Button
+        Me.Button14 = New System.Windows.Forms.Button
+        Me.Button15 = New System.Windows.Forms.Button
+        Me.Div = New System.Windows.Forms.Button
+        Me.Button17 = New System.Windows.Forms.Button
+        Me.Button12 = New System.Windows.Forms.Button
+        Me.Button16 = New System.Windows.Forms.Button
+        Me.SuspendLayout()
+        '
+        'LB
+        '
+        Me.LB.BackColor = System.Drawing.SystemColors.Window
+        Me.LB.FormattingEnabled = True
+        resources.ApplyResources(Me.LB, "LB")
+        Me.LB.Name = "LB"
+        '
+        'Button1
+        '
+        resources.ApplyResources(Me.Button1, "Button1")
+        Me.Button1.Name = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        resources.ApplyResources(Me.Button2, "Button2")
+        Me.Button2.Name = "Button2"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'sqrt
+        '
+        resources.ApplyResources(Me.sqrt, "sqrt")
+        Me.sqrt.Name = "sqrt"
+        Me.sqrt.UseVisualStyleBackColor = True
+        '
+        'pow
+        '
+        resources.ApplyResources(Me.pow, "pow")
+        Me.pow.Name = "pow"
+        Me.pow.UseVisualStyleBackColor = True
+        '
+        'log
+        '
+        resources.ApplyResources(Me.log, "log")
+        Me.log.Name = "log"
+        Me.log.UseVisualStyleBackColor = True
+        '
+        'sin
+        '
+        resources.ApplyResources(Me.sin, "sin")
+        Me.sin.Name = "sin"
+        Me.sin.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        resources.ApplyResources(Me.TextBox1, "TextBox1")
+        Me.TextBox1.Name = "TextBox1"
+        '
+        'Button3
+        '
+        resources.ApplyResources(Me.Button3, "Button3")
+        Me.Button3.Name = "Button3"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Cos
+        '
+        resources.ApplyResources(Me.Cos, "Cos")
+        Me.Cos.Name = "Cos"
+        Me.Cos.UseVisualStyleBackColor = True
+        '
+        'Asin
+        '
+        resources.ApplyResources(Me.Asin, "Asin")
+        Me.Asin.Name = "Asin"
+        Me.Asin.UseVisualStyleBackColor = True
+        '
+        'Ctan
+        '
+        resources.ApplyResources(Me.Ctan, "Ctan")
+        Me.Ctan.Name = "Ctan"
+        Me.Ctan.UseVisualStyleBackColor = True
+        '
+        'Fact
+        '
+        resources.ApplyResources(Me.Fact, "Fact")
+        Me.Fact.Name = "Fact"
+        Me.Fact.UseVisualStyleBackColor = True
+        '
+        'Button8
+        '
+        resources.ApplyResources(Me.Button8, "Button8")
+        Me.Button8.Name = "Button8"
+        Me.Button8.UseVisualStyleBackColor = True
+        '
+        'Button9
+        '
+        resources.ApplyResources(Me.Button9, "Button9")
+        Me.Button9.Name = "Button9"
+        Me.Button9.UseVisualStyleBackColor = True
+        '
+        'Acos
+        '
+        resources.ApplyResources(Me.Acos, "Acos")
+        Me.Acos.Name = "Acos"
+        Me.Acos.UseVisualStyleBackColor = True
+        '
+        'Second
+        '
+        resources.ApplyResources(Me.Second, "Second")
+        Me.Second.Name = "Second"
+        '
+        'Powx
+        '
+        resources.ApplyResources(Me.Powx, "Powx")
+        Me.Powx.Name = "Powx"
+        Me.Powx.UseVisualStyleBackColor = True
+        '
+        'nsqrt
+        '
+        resources.ApplyResources(Me.nsqrt, "nsqrt")
+        Me.nsqrt.Name = "nsqrt"
+        Me.nsqrt.UseVisualStyleBackColor = True
+        '
+        'Sum
+        '
+        resources.ApplyResources(Me.Sum, "Sum")
+        Me.Sum.Name = "Sum"
+        Me.Sum.UseVisualStyleBackColor = True
+        '
+        'Button7
+        '
+        resources.ApplyResources(Me.Button7, "Button7")
+        Me.Button7.Name = "Button7"
+        Me.Button7.UseVisualStyleBackColor = True
+        '
+        'Button10
+        '
+        resources.ApplyResources(Me.Button10, "Button10")
+        Me.Button10.Name = "Button10"
+        Me.Button10.UseVisualStyleBackColor = True
+        '
+        'ABS
+        '
+        resources.ApplyResources(Me.ABS, "ABS")
+        Me.ABS.Name = "ABS"
+        Me.ABS.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        resources.ApplyResources(Me.Button5, "Button5")
+        Me.Button5.Name = "Button5"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        resources.ApplyResources(Me.Button6, "Button6")
+        Me.Button6.Name = "Button6"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Button11
+        '
+        resources.ApplyResources(Me.Button11, "Button11")
+        Me.Button11.Name = "Button11"
+        Me.Button11.UseVisualStyleBackColor = True
+        '
+        'Button13
+        '
+        resources.ApplyResources(Me.Button13, "Button13")
+        Me.Button13.Name = "Button13"
+        Me.Button13.UseVisualStyleBackColor = True
+        '
+        'Button14
+        '
+        resources.ApplyResources(Me.Button14, "Button14")
+        Me.Button14.Name = "Button14"
+        Me.Button14.UseVisualStyleBackColor = True
+        '
+        'Button15
+        '
+        resources.ApplyResources(Me.Button15, "Button15")
+        Me.Button15.Name = "Button15"
+        Me.Button15.UseVisualStyleBackColor = True
+        '
+        'Div
+        '
+        resources.ApplyResources(Me.Div, "Div")
+        Me.Div.Name = "Div"
+        Me.Div.UseVisualStyleBackColor = True
+        '
+        'Button17
+        '
+        resources.ApplyResources(Me.Button17, "Button17")
+        Me.Button17.Name = "Button17"
+        Me.Button17.UseVisualStyleBackColor = True
+        '
+        'Button12
+        '
+        resources.ApplyResources(Me.Button12, "Button12")
+        Me.Button12.Name = "Button12"
+        Me.Button12.UseVisualStyleBackColor = True
+        '
+        'Button16
+        '
+        resources.ApplyResources(Me.Button16, "Button16")
+        Me.Button16.Name = "Button16"
+        Me.Button16.UseVisualStyleBackColor = True
+        '
+        'Vahe_app
+        '
+        resources.ApplyResources(Me, "$this")
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.Controls.Add(Me.Button16)
+        Me.Controls.Add(Me.Button12)
+        Me.Controls.Add(Me.Button17)
+        Me.Controls.Add(Me.Button15)
+        Me.Controls.Add(Me.Div)
+        Me.Controls.Add(Me.Button14)
+        Me.Controls.Add(Me.Button13)
+        Me.Controls.Add(Me.Button11)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.ABS)
+        Me.Controls.Add(Me.Button10)
+        Me.Controls.Add(Me.Button7)
+        Me.Controls.Add(Me.Sum)
+        Me.Controls.Add(Me.Powx)
+        Me.Controls.Add(Me.nsqrt)
+        Me.Controls.Add(Me.Acos)
+        Me.Controls.Add(Me.Second)
+        Me.Controls.Add(Me.Button9)
+        Me.Controls.Add(Me.Button8)
+        Me.Controls.Add(Me.Fact)
+        Me.Controls.Add(Me.Ctan)
+        Me.Controls.Add(Me.Cos)
+        Me.Controls.Add(Me.Asin)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.log)
+        Me.Controls.Add(Me.pow)
+        Me.Controls.Add(Me.sin)
+        Me.Controls.Add(Me.sqrt)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.LB)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D
+        Me.Name = "Vahe_app"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents LB As System.Windows.Forms.ListBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents sqrt As System.Windows.Forms.Button
+    Friend WithEvents pow As System.Windows.Forms.Button
+    Friend WithEvents log As System.Windows.Forms.Button
+    Friend WithEvents sin As System.Windows.Forms.Button
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents Cos As System.Windows.Forms.Button
+    Friend WithEvents Asin As System.Windows.Forms.Button
+    Friend WithEvents Ctan As System.Windows.Forms.Button
+    Friend WithEvents Fact As System.Windows.Forms.Button
+    Friend WithEvents Button8 As System.Windows.Forms.Button
+    Friend WithEvents Button9 As System.Windows.Forms.Button
+    Friend WithEvents Acos As System.Windows.Forms.Button
+    Friend WithEvents Second As System.Windows.Forms.TextBox
+    Friend WithEvents Powx As System.Windows.Forms.Button
+    Friend WithEvents nsqrt As System.Windows.Forms.Button
+    Friend WithEvents Sum As System.Windows.Forms.Button
+    Friend WithEvents Button7 As System.Windows.Forms.Button
+    Friend WithEvents Button10 As System.Windows.Forms.Button
+    Friend WithEvents ABS As System.Windows.Forms.Button
+    Friend WithEvents Button5 As System.Windows.Forms.Button
+    Friend WithEvents Button6 As System.Windows.Forms.Button
+    Friend WithEvents Button11 As System.Windows.Forms.Button
+    Friend WithEvents Button13 As System.Windows.Forms.Button
+    Friend WithEvents Button14 As System.Windows.Forms.Button
+    Friend WithEvents Button15 As System.Windows.Forms.Button
+    Friend WithEvents Div As System.Windows.Forms.Button
+    Friend WithEvents Button17 As System.Windows.Forms.Button
+    Friend WithEvents Button12 As System.Windows.Forms.Button
+    Friend WithEvents Button16 As System.Windows.Forms.Button
+
+End Class
---
+++ Source/Form1.vb
@@ -0,0 +1,263 @@
+﻿' Microsoft Visual basic 2008 pro
+' ******************************************************************
+' Copyright © Vahe Danielyan 2014 <danielyan.vahe@gmail.com> 
+'
+' Everyone is permitted to copy and distribute verbatim or modified
+' copies of this license document, and changing it is allowed as long
+' as the name is changed.
+'
+'********************************************************************
+Public Class Vahe_app
+    Public Final As Integer
+    Public S As String
+    Public str As String
+    Public Final2 As Integer
+    '////////////////////////////'Input from textbox'////////////////////////////////////////////////////////////////////////////
+    Private Sub Init(ByVal TextBox1 As System.Object)
+        S = TextBox1.text
+        If S = "" Then
+            Final = 0
+            Return
+        End If
+        Final = CType(S, Integer)
+    End Sub
+    '/////////////////////////////////'Input from textbox'//////////////////////////////////////////////////////////////////////
+    Private Sub Init2(ByVal Second As System.Object)
+        str = Second.text
+        If str = "" Then
+            Final2 = 0
+            Return
+        End If
+        Final2 = CType(str, Integer)
+    End Sub
+    '////////////////////////////////////'Application is being loaded here'//////////////////////////////////////////////////////
+    Private Sub Vahe_app_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+    End Sub
+    '////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    Private Sub sqrt_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles sqrt.Click
+        Init(TextBox1)
+        LB.Items.Add(Math.Sqrt(Final))
+    End Sub
+    '////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    Private Sub pow_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles pow.Click
+        Init(TextBox1)
+        LB.Items.Add(Math.Pow(Final, 2))
+    End Sub
+    '///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    Private Sub log_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles log.Click
+        Init(TextBox1)
+        LB.Items.Add(Math.Log10(Final))
+    End Sub
+    '////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    Private Sub sin_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles sin.Click
+        Init(TextBox1)
+        LB.Items.Add(Math.Sin(Final))
+    End Sub
+    '///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        LB.Items.Clear()
+    End Sub
+    '////////////////////////////////////////////////////////'Application closing part'/////////////////////////////////////////
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        If MessageBox.Show("Are you sure you want to quit app", "Quit", MessageBoxButtons.YesNo, MessageBoxIcon.Question) = Windows.Forms.DialogResult.Yes Then
+            Close()
+        End If
+
+    End Sub
+    '///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    Private Sub TextBox1_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles TextBox1.TextChanged
+    End Sub
+    '///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    Private Sub Cos_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Cos.Click
+        Init(TextBox1)
+        LB.Items.Add(Math.Cos(Final))
+    End Sub
+    '///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
+        Init(TextBox1)
+        LB.Items.Add(Math.Tan(Final))
+    End Sub
+    '///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    Private Sub Ctan_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Ctan.Click
+        Init(TextBox1)
+        LB.Items.Add(1 / (Math.Tan(Final)))
+    End Sub
+    '///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    Private Sub Asin_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Asin.Click
+        Init(TextBox1)
+        LB.Items.Add(Math.Asin(Final))
+    End Sub
+    '///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    Private Sub Acos_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Acos.Click
+        Init(TextBox1)
+        LB.Items.Add(Math.Acos(Final))
+    End Sub
+    '///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    Private Sub Fact_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Fact.Click
+        Init(TextBox1)
+
+        If Final > 20 Then
+            LB.Items.Add("NaN")
+            Return
+        End If
+
+        Dim fact As Int64 = 1
+        For i As Integer = 2 To Final
+            fact = fact * i
+        Next
+        LB.Items.Add(fact)
+    End Sub
+    '///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    Private Sub Button8_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button8.Click
+        LB.Items.Add(Math.PI)
+    End Sub
+    '////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    Private Sub TextBox2_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Second.TextChanged
+    End Sub
+    '////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    Private Sub Button9_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button9.Click
+        Init2(Second)
+        Init(TextBox1)
+        LB.Items.Add(Math.Log(Final, Final2))
+    End Sub
+    '////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    Private Sub Powx_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Powx.Click
+        Init(TextBox1)
+        Init(Second)
+        LB.Items.Add(Math.Pow(Final, Final2))
+    End Sub
+    '////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    Private Sub nsqrt_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles nsqrt.Click
+        Init(TextBox1)
+        Init2(Second)
+        LB.Items.Add(Math.Pow(Final, (1 / Final2)))
+    End Sub
+    '///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    Private Sub Msc_Click(ByVal sender As System.Object, ByVal e As System.EventArgs)
+        'My.Computer.Audio.Play(My.Resources.RapGod, AudioPlayMode.BackgroundLoop)
+    End Sub
+
+    Private Sub An_Click(ByVal sender As System.Object, ByVal e As System.EventArgs)
+        'My.Computer.Audio.Play(My.Resources.Animals, AudioPlayMode.BackgroundLoop)
+    End Sub
+
+    Private Sub Button4_Click(ByVal sender As System.Object, ByVal e As System.EventArgs)
+        My.Computer.Audio.Stop()
+    End Sub
+
+    Private Sub LP_Click(ByVal sender As System.Object, ByVal e As System.EventArgs)
+    End Sub
+
+    Private Sub Sum_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Sum.Click
+        Init(TextBox1)
+        LB.Items.Add(((Final) * (Final + 1)) / 2)
+    End Sub
+
+    Private Sub Button7_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button7.Click
+        Init(TextBox1)
+        Init2(Second)
+        If Final = 0 Then
+            LB.Items.Add("NaN")
+            Return
+        End If
+        LB.Items.Add(Final Mod Final2)
+    End Sub
+
+    Private Sub Button10_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button10.Click
+        Init(TextBox1)
+        Init2(Second)
+        LB.Items.Add(Final * Final2)
+    End Sub
+
+    Private Sub ABS_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ABS.Click
+        Init(TextBox1)
+        LB.Items.Add(Math.Abs(Final))
+    End Sub
+
+    Private Sub Button5_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button5.Click
+        Init(TextBox1)
+        Init2(Second)
+        LB.Items.Add(Final * Final2 / 100)
+    End Sub
+
+    Private Sub Button6_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button6.Click
+        LB.Items.Add(Math.E)
+    End Sub
+
+    Private Sub Button11_Click(ByVal sender As System.Object, ByVal e As System.EventArgs)
+        LB.Items.Add(9.8)
+    End Sub
+
+    Private Sub Button11_Click_1(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button11.Click
+        Init(TextBox1)
+        Init2(Second)
+        LB.Items.Add(Math.Atan(1 / Math.Tan(Final)))
+    End Sub
+
+    Private Sub Button13_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button13.Click
+        Init(TextBox1)
+        Init(Second)
+        LB.Items.Add(Math.Atan(Final))
+    End Sub
+
+    Private Sub Button14_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button14.Click
+        Init(TextBox1)
+        Init2(Second)
+        Dim c As Integer
+        While Final2 <> 0
+            Final = Final Mod Final2
+            c = Final
+            Final = Final2
+            Final2 = c
+        End While
+        LB.Items.Add(Final)
+    End Sub
+
+    Private Sub Div_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Div.Click
+        Init(TextBox1)
+        Init2(Second)
+        LB.Items.Add(Final / Final2)
+    End Sub
+
+    Private Sub Button12_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button12.Click
+        Init(TextBox1)
+        Init2(Second)
+        LB.Items.Add(Final + Final2)
+    End Sub
+
+    Private Sub Button16_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button16.Click
+        Init(TextBox1)
+        Init2(Second)
+        LB.Items.Add(Final - Final2)
+    End Sub
+    Private Sub ViewToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs)
+
+    End Sub
+
+    Private Sub MainToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs)
+    End Sub
+
+    Private Sub LB_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles LB.SelectedIndexChanged
+
+    End Sub
+
+    Private Sub Button15_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button15.Click
+        Init(TextBox1)
+        Init2(Second)
+        Dim k As Integer
+        k = Final * Final2
+        Dim c As Integer
+        While Final2 <> 0
+            Final = Final Mod Final2
+            c = Final
+            Final = Final2
+            Final2 = c
+        End While
+        LB.Items.Add(k / Final)
+    End Sub
+
+    Private Sub Button17_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button17.Click
+        Init(TextBox1)
+        LB.Items.Add(Math.Exp(Final))
+    End Sub
+End Class
